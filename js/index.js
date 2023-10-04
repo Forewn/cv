60a8c328674d84eeecb41e377e134ca85b667009
Jhosmar Suarez <jhosmardsuarezc961@gmail.com>
2023-10-04 00:30:38 -0400

    entregar

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
-const bar = document.querySelector('.contenedor_home');
+const bar = document.querySelector('header');
+const img = document.querySelector('.contenedor_home');
 let prevY = window.scrollY;
+
 window.addEventListener('scroll', ()=>{
-    if(prevY > window.scrollY){
-        console.log("subir");
-        bar.classList.remove('off');
-    }
-    else{
-        console.log('bajar');
+    if(window.scrollY > prevY && window.scrollY >= 280){
         bar.classList.add('off');
+        img.classList.add('contenedor_home_hidden');
+    }
+    else if(window.scrollY < prevY && window.scrollY == 0){
+        bar.classList.remove('off');
+        img.classList.remove('contenedor_home_hidden');
     }
     prevY = window.scrollY;
+    console.log(prevY);
 });
